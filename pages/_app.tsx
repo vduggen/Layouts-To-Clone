e197c20dd9188a3308bd129e3ce3e08975fbad85
Vitor Duggen <zduggem@gmail.com>
2020-11-01 19:48:35 -0300

    Add Tag Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,22 @@ import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
 import "../styles/reset.css";
 import "react-toastify/dist/ReactToastify.min.css";
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
+			<Head>
+				<!-- Global site tag (gtag.js) - Google Analytics -->
+				<script async src="https://www.googletagmanager.com/gtag/js?id=G-BDE2GLXLNC"></script>
+				<script>
+					window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+
+					gtag('config', 'G-BDE2GLXLNC');
+				</script>
+			</Head>
 			<Component {...pageProps} />
 			<ToastContainer />
 		</>
