dbebb1453a08d9e3ed366453968a6c65659b057a
vduggen <zduggem@gmail.com>
2020-11-01 19:59:57 -0300

    Add React-GA Dependence and initialize

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,11 @@ import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
 import "../styles/reset.css";
 import "react-toastify/dist/ReactToastify.min.css";
+import ReactGA from "react-ga";
 
 function MyApp({ Component, pageProps }: AppProps) {
+	ReactGA.initialize("G-BDE2GLXLNC");
+
 	return (
 		<>
 			<Component {...pageProps} />
