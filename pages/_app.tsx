458f384ad18f510eca4326c1f3536d418eb3ef26
Vitor Duggen <zduggem@gmail.com>
2020-11-02 11:39:31 -0300

    Add config ReactGA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import { useEffect } from 'react';
 import { ToastContainer } from "react-toastify";
 import "../styles/reset.css";
 import "react-toastify/dist/ReactToastify.min.css";
@@ -6,6 +7,10 @@ import ReactGA from "react-ga";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	ReactGA.initialize("G-C1PH8X16BQ");
+	
+	useEffect(() => {
+		ReactGA.pageview(window.location.pathname + window.location.search);
+	}, [])
 
 	return (
 		<>
